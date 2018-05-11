bdb1fe16ce6dcc3ba07a9415356f127bb01aff99
Byeongguk Gong <byeonggukgong@gmail.com>
2018-05-11 16:15:56 +0900

    Update BOJ algorithm
    * Update boj/11727.py
    
    * Update README.md

---
+++ boj/11727.py
@@ -0,0 +1,10 @@
+# -*- coding: utf -*-
+
+if __name__ == '__main__':
+    input = int(input())
+    output = [1, 3]
+    
+    for x in range(2, input):
+        output.append(output[x - 2] * 2 + output[x - 1])
+    
+    print(output[input - 1] % 10007)
